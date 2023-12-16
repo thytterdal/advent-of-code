7772636225ac8216337b68c931477375930c6579
Thomas Ytterdal <thytterdal@gmail.com>
2023-12-16 09:11:16 +0100

    Add coroutines

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.21"
 }
 
 group = "dev.ytterdal"
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
     testImplementation(kotlin("test"))
 }
 
