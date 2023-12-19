8553c43ffa242b25a48ccd2f32190207c56f2090
Thomas Ytterdal <thytterdal@gmail.com>
2023-12-19 21:27:15 +0100

    day 19 part 2

--- src/main/kotlin/day19/day19.kt
+++ src/main/kotlin/day19/day19.kt
@@ -1,8 +1,9 @@
 package day19
 
-
 import utils.readInput
 import utils.splitOnEmpty
+import kotlin.math.max
+import kotlin.math.min
 
 fun main() {
     val lines = readInput("day19/input")
@@ -25,9 +26,49 @@ private fun firstStar(lines: List<String>) {
 }
 
 private fun secondStar(lines: List<String>) {
-    val sum = 0
+    val (_, workflows) = lines.parseInput()
+
+    val sum = workflows.acceptedWorkFlows().map { steps ->
+        steps.groupBy(
+            keySelector = { it.property }
+        ) {
+            it.validRange
+        }.mapValues { (_, value) -> value.reduce { acc, validRange -> acc.intersect(validRange) } }
+            .withDefault { 1L..4000L }
+    }.sumOf {
+        val x = it.getValue(PartProperty.X).length()
+        val m = it.getValue(PartProperty.M).length()
+        val a = it.getValue(PartProperty.A).length()
+        val s = it.getValue(PartProperty.S).length()
+        x * m * a * s
+    }
 
     println("Second ⭐: $sum")
+}
+
+
+private fun Map<String, List<WorkFlowStep>>.acceptedWorkFlows(
+    stepName: String = "in",
+    stepsSoFar: List<WorkFlowStep> = emptyList()
+): List<List<WorkFlowStepWithCondition>> = buildList {
+    val steps = this@acceptedWorkFlows[stepName] ?: throw Exception("Unknown step $stepName")
+    val previous = mutableListOf<WorkFlowStepWithCondition>()
+    for (step in steps) {
+        when (step.destination) {
+            "A" -> add((stepsSoFar + previous + step).filterIsInstance<WorkFlowStepWithCondition>())
+            "R" -> Unit
+            else -> addAll(
+                this@acceptedWorkFlows.acceptedWorkFlows(
+                    stepName = step.destination,
+                    stepsSoFar = (stepsSoFar + previous + step)
+                )
+            )
+        }
+
+        if (step is WorkFlowStepWithCondition) {
+            previous += step.invert()
+        }
+    }
 }
 
 private fun Map<String, List<WorkFlowStep>>.checkCondition(stepName: String, part: Part): String {
@@ -48,13 +89,8 @@ private fun Map<String, List<WorkFlowStep>>.checkCondition(stepName: String, par
                     PartProperty.A -> part.a
                     PartProperty.S -> part.s
                 }
-                val condition = if (step.comparator == WorkFlowComparator.LT) {
-                    value < step.value
-                } else {
-                    value > step.value
-                }
 
-                if (condition) {
+                if (value in step.validRange) {
                     return when (step.destination) {
                         "A" -> "A"
                         "R" -> "R"
@@ -95,11 +131,15 @@ private fun List<String>.parseInput(): Pair<List<Part>, Map<String, List<WorkFlo
             .map { step ->
                 if (step.contains(":")) {
                     val (condition, destination) = step.split(":")
-                    val (property, value) = condition.split("<", ">")
+                    val (property, value) = condition.split("<", ">").let { it.first() to it.last().toLong() }
                     WorkFlowStepWithCondition(
                         property = PartProperty.valueOf(property.uppercase()),
-                        value = value.toLong(),
-                        comparator = if (condition.contains("<")) WorkFlowComparator.LT else WorkFlowComparator.GT,
+                        value = value,
+                        validRange = if (condition.contains("<")) {
+                            1L..<value
+                        } else {
+                            (value + 1)..4000L
+                        },
                         destination = destination
                     )
                 } else {
@@ -126,22 +166,29 @@ private data class Part(
     }
 }
 
-private sealed interface WorkFlowStep
+private fun LongRange.invert() = if (first == 1L) (last + 1)..4000L else 1L..<first
+private fun LongRange.intersect(other: LongRange) = max(first, other.first)..min(last, other.last)
+private fun LongRange.length() = last - first + 1
+
+private sealed interface WorkFlowStep {
+    val destination: String
+}
+
 data class WorkFlowStepWithCondition(
     val property: PartProperty,
     val value: Long,
-    val comparator: WorkFlowComparator,
-    val destination: String
-) : WorkFlowStep
+    val validRange: LongRange,
+    override val destination: String
+) : WorkFlowStep {
+    fun invert() = this.copy(
+        validRange = validRange.invert()
+    )
+}
 
 data class Destination(
-    val destination: String
+    override val destination: String
 ) : WorkFlowStep
 
-
-enum class WorkFlowComparator {
-    LT, GT
-}
 
 enum class PartProperty {
     X, M, A, S
