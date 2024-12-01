b8659dcb7e0c8441ebb1594fb9d6d0df0eb9d907
Thomas Ytterdal <thomas@ytterdal.dev>
2024-12-01 09:04:12 +0100

    2024 day 1

--- src/main/kotlin/aoc2024/Day1.kt
+++ src/main/kotlin/aoc2024/Day1.kt
@@ -1,15 +1,41 @@
 package aoc2024
 
 import common.Challenge
+import kotlin.math.abs
 
 private val aoc2024day1 = object : Challenge(year = 2024, day = 1) {
     override fun silverStar(lines: List<String>): Long {
-        TODO("Not yet implemented")
+        val (left, right) = lines.splitInput()
+
+        val accumulatedDiff = left.foldIndexed(0L) { index, acc, l ->
+            val diff = abs(l - right[index])
+            acc + diff
+        }
+
+        return accumulatedDiff
     }
 
     override fun goldStar(lines: List<String>): Long {
-        TODO("Not yet implemented")
+        val (left, right) = lines.splitInput()
+
+        val accumulatedDiff = left.fold(0L) { acc, l ->
+            val occurences = right.count { it == l }
+            val similarityScore = l * occurences
+            acc + similarityScore
+        }
+
+        return accumulatedDiff
     }
+}
+
+private fun List<String>.splitInput(): Pair<List<Long>, List<Long>> {
+    val split = associate { line ->
+        val split = line.split(" ")
+        split.first().toLong() to split.last().toLong()
+    }
+    val left = split.keys.sorted()
+    val right = split.values.sorted()
+    return left to right
 }
 
 
