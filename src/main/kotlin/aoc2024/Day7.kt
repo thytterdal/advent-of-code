18065fecf4f49ee93f629c1dfb71d11b9743192e
Thomas Ytterdal <thomas@ytterdal.dev>
2024-12-07 09:01:25 +0100

    rename parameter

--- src/main/kotlin/aoc2024/Day7.kt
+++ src/main/kotlin/aoc2024/Day7.kt
@@ -3,6 +3,7 @@ package aoc2024
 import common.Challenge
 
 val aoc2024day7 = object : Challenge(year = 2024, day = 7) {
+
     override fun silverStar(lines: List<String>): Long {
         val equations = lines.map { line ->
             line.substringBefore(":").toLong() to line.substringAfter(": ").split(" ").map { num -> num.toLong() }
@@ -27,7 +28,7 @@ val aoc2024day7 = object : Challenge(year = 2024, day = 7) {
 fun List<Long>.solve(sum: Long): List<Long> {
     if (isEmpty()) return listOf(sum)
     return drop(1).solve(sum + first()) +
-           drop(1).solve(sum * first())
+            drop(1).solve(sum * first())
 }
 
 fun List<Long>.solveWithConcatenation(sum: Long): List<Long> {
