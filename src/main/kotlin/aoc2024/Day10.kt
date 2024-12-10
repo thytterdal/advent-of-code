f9493cd632971ec795a2b94a4b0b6c097e55726f
Thomas Ytterdal <thomas@ytterdal.dev>
2024-12-10 21:58:38 +0100

    Formatting

--- src/main/kotlin/aoc2024/Day10.kt
+++ src/main/kotlin/aoc2024/Day10.kt
@@ -1,7 +1,6 @@
 package aoc2024
 
 import common.Challenge
-import utils.Point
 import utils.toGrid
 
 private val aoc2024day10 = object : Challenge(year = 2024, day = 10) {
@@ -11,8 +10,8 @@ private val aoc2024day10 = object : Challenge(year = 2024, day = 10) {
 
         val trailScores = trailheads.map { startPoint ->
             (1..9).fold(listOf(startPoint)) { acc, i ->
-                    acc.flatMap { pos -> pos.neighbors().filter { grid[it] == i } }.distinct()
-                }.count()
+                acc.flatMap { pos -> pos.neighbors().filter { grid[it] == i } }.distinct()
+            }.count()
         }
         return trailScores.sum().toLong()
     }
@@ -21,12 +20,12 @@ private val aoc2024day10 = object : Challenge(year = 2024, day = 10) {
         val grid = lines.toGrid { it.digitToInt() }
         val trailheads = grid.filterValues { it == 0 }.map { it.key }
 
-        val trailScrores = trailheads.map { startPoint ->
+        val trailScores = trailheads.map { startPoint ->
             (1..9).fold(listOf(startPoint)) { acc, i ->
-                    acc.flatMap { pos -> pos.neighbors().filter { grid[it] == i } }
-                }.count()
+                acc.flatMap { pos -> pos.neighbors().filter { grid[it] == i } }
+            }.count()
         }
-        return trailScrores.sum().toLong()
+        return trailScores.sum().toLong()
     }
 }
 
